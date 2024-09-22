b66809540ede88636cb374dc32a0d395afb5a071
Murat Okutucu <mokutucu@c3a8c5.42berlin.de>
2024-09-22 17:24:21 +0200

    added philo

---
+++ philosophers/include/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 17:15:26 by mokutucu          #+#    #+#             */
+/*   Updated: 2024/09/22 17:16:00 by mokutucu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+
+# endif
