38229e396646399f62e734ffbf7a838a4da3df47
Murat Okutucu <moktu123@gmail.com>
2024-09-25 21:57:59 +0200

    p

--- philosophers/includes/philo.h
+++ philosophers/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 17:15:26 by mokutucu          #+#    #+#             */
-/*   Updated: 2024/09/24 18:17:22 by mokutucu         ###   ########.fr       */
+/*   Updated: 2024/09/25 21:04:59 by mokutucu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <sys/time.h>
 # include <string.h>
 
+// Struct for each individual philosopher
 typedef struct s_philo
 {
     int philo_id;
@@ -27,8 +28,22 @@ typedef struct s_philo
     int time_to_eat;
     int time_to_sleep;
     int number_of_eats;
-    int left_fork;
-    int right_fork;
+    pthread_t thread;
+    pthread_mutex_t left_fork;
+    pthread_mutex_t right_fork;
 }              t_philo;
+
+// Struct for main thread aka. the table
+typedef struct s_table
+{
+    int number_of_philosophers;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int must_eats;
+    t_philo *philo;
+}              t_table;
+
+int	ft_atoi(const char *str);
 
 # endif
--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -5,28 +5,116 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mokutucu <mokutucu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/24 17:44:10 by mokutucu          #+#    #+#             */
-/*   Updated: 2024/09/24 18:07:45 by mokutucu         ###   ########.fr       */
+/*   Created: 2024/09/25 15:09:05 by mokutucu          #+#    #+#             */
+/*   Updated: 2024/09/25 21:57:13 by mokutucu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void init_philo(t_philo *philo)
+int validate_input(int argc, char **argv)
 {
-    philo->philo_id = 1;
-    philo->time_to_die = 2;
-    philo->time_to_eat = 3;
-    philo->time_to_sleep = 4;
-    philo->number_of_eats = 5;
+    if (argc != 6)
+    {
+        printf("Error: Wrong number of arguments\n");
+        return (1);
+    }
+    if (ft_atoi(argv[1]) < 1)
+    {
+        printf("Philosophers must be at least 1\n");
+        return (1);   
+    }
+    return (0);
 }
 
-int main(void)
+void init_table(t_table *table, t_philo **philo, char **argv)
 {
+    // Zero out the table struct
+    memset(table, 0, sizeof(t_table)); 
+    table->number_of_philosophers = ft_atoi(argv[1]);
+    table->time_to_die = ft_atoi(argv[2]);
+    table->time_to_eat = ft_atoi(argv[3]);
+    table->time_to_sleep = ft_atoi(argv[4]);
+    table->must_eats = ft_atoi(argv[5]);
+
+    // Allocate memory for philosophers and zero it out
+    *philo = malloc(sizeof(t_philo) * table->number_of_philosophers); 
+    if (!*philo)
+    {
+        printf("Error: Memory allocation failed for philosophers\n");
+        exit(1);
+    }
+    
+    // Zero out the philosophers array
+    memset(*philo, 0, sizeof(t_philo) * table->number_of_philosophers);
+    
+    // Assign the pointer to the table struct
+    table->philo = *philo;
+}
+
+pthread_mutex_t print_mutex;
+
+void *philo_routine(void *arg)
+{
+    t_philo *philo = (t_philo *)arg;
+
+    pthread_mutex_lock(&print_mutex);
+    printf("Philosopher %d is thinking\n", philo->philo_id);
+    pthread_mutex_unlock(&print_mutex);
+
+    usleep(philo->time_to_eat * 1000); // Simulate thinking and eating
+
+    pthread_mutex_lock(&print_mutex);
+    printf("Philosopher %d is eating\n", philo->philo_id);
+    pthread_mutex_unlock(&print_mutex);
+
+    usleep(philo->time_to_sleep * 1000); // Simulate sleeping
+
+    return NULL;
+}
+
+void create_threads(t_table *table, t_philo *philo)
+{
+    int i;
+
+    i = 0;
+    while (i < table->number_of_philosophers)
+    {
+        philo[i].philo_id = i + 1;
+        philo[i].time_to_die = table->time_to_die;
+        philo[i].time_to_eat = table->time_to_eat;
+        philo[i].time_to_sleep = table->time_to_sleep;
+        philo[i].number_of_eats = table->must_eats;
+        pthread_create(&philo[i].thread, NULL, philo_routine, &philo[i]);
+        i++;
+    }
+
+    // Wait for all threads to finish
+    i = 0;
+    while (i < table->number_of_philosophers)
+    {
+        pthread_join(philo[i].thread, NULL);
+        i++;
+    }
+}
+
+// argv: [number of philosophers, time to die, time to eat, time to sleep, number of eats]
+int main(int argc, char **argv)
+{
+    t_table table;
     t_philo *philo;
     
-    philo = (t_philo *)malloc(sizeof(t_philo));
-    init_philo(philo);
-    printf("philo_id: %d\n", philo->philo_id);
+    if (validate_input(argc, argv))
+        return (1);
+    
+    init_table(&table, &philo, argv);
+    
+    pthread_mutex_init(&print_mutex, NULL);
+    create_threads(&table, table.philo);
+
+    printf("All philosophers have finished.\n");
+    pthread_mutex_destroy(&print_mutex); // Destroy mutex
+
+    free(table.philo);
     return (0);
 }
